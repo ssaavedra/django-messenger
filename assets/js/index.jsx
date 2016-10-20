f8c62fc2c7d47f7b04fb6aca756310c8900ae3a6
Santiago Saavedra <santiagosaavedra@gmail.com>
2016-10-21 01:20:12 +0200

    Use browserHistory, Django is OK with that

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { Router, hashHistory } from 'react-router'
+import { Router, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import routes from './routes'
 import configureStore from './store/configureStore'
@@ -14,7 +14,7 @@ require('./favicon.ico') // Tell webpack to load favicon.ico
 const store = configureStore()
 
 // Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(hashHistory, store)
+const history = syncHistoryWithStore(browserHistory, store)
 
 render(
   <Provider store={store}>
