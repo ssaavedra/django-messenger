3f5c7a612f00f37b418db1b5e9394cd20ecd0a7a
Santiago Saavedra <santiagosaavedra@gmail.com>
2016-10-21 00:53:37 +0200

    Add models to Django Admin

--- djmessenger/admin.py
+++ djmessenger/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.UserProfile)
+@admin.register(models.ChatRoom)
+@admin.register(models.Message)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
