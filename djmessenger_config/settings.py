6477f1e56af0842e4a31bb7f38c6712aaed730fc
Santiago Saavedra <santiagosaavedra@gmail.com>
2016-10-20 19:59:21 +0200

    Add django-webpack-loader to use React in front

--- djmessenger_config/settings.py
+++ djmessenger_config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webpack_loader',
     'djmessenger',
 ]
 
@@ -119,3 +120,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    # We do this so that django's collectstatic copies our bundles to the
+    # STATIC_ROOT or syncs them to whatever storage we use
+    os.path.join(BASE_DIR, 'assets'),
+)
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+    }
+}
