b1940484c3bf1ac2a7c78b7d596042ce2d8cf587
Santiago Saavedra <s.saavedra@fdi.ucm.es>
2016-10-21 12:19:03 +0200

    Create the UserProfile model as soon as the user is registered

--- djmessenger/models.py
+++ djmessenger/models.py
@@ -1,5 +1,7 @@
 from django.conf import settings
+from django.dispatch import receiver
 from django.db import models
+from django.db.models.signals import post_save
 from django.utils import timezone
 
 
@@ -43,3 +45,7 @@ class Message(models.Model):
             sender=self.sender,
             timestamp=self.timestamp,
         )
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def user_saved_create_profile(sender, instance, **kwargs):
+    UserProfile.objects.get_or_create(user=instance)
